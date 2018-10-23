229a01881b21d6cab1ffcc1ba4e04e96e27e48f2
Lawrence <lawrenslava1@gmail.com>
2018-10-23 17:00:50 +0300

    Finish design of the login screen

--- app/src/main/java/com/lawrence254/moringa/loginActivity.java
+++ app/src/main/java/com/lawrence254/moringa/loginActivity.java
@@ -3,12 +3,11 @@ package com.lawrence254.moringa;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class loginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        setTheme(R.style.splashscreen);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
