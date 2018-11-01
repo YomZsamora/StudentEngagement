029fe67c6b67d964a38d42c1839e272caa290bd1
yomz <samora.y@adzumi.co.ke>
2018-11-01 07:46:20 +0300

    Added Bottom AppBar.

--- app/src/main/java/com/adzumi/studentengagement/ui/MainActivity.java
+++ app/src/main/java/com/adzumi/studentengagement/ui/MainActivity.java
@@ -3,12 +3,16 @@ package com.adzumi.studentengagement.ui;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.adzumi.studentengagement.R;
 
@@ -18,17 +22,34 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.my_toolbar) Toolbar myToolbar;
+    @BindView(R.id.bottom_navigation) BottomNavigationView mBottom_navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.logo_appbar);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        mBottom_navigation.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+               switch (item.getItemId())
+               {
+                   case R.id.action_home:
+                       Toast.makeText(MainActivity.this, "Home Add Clicked", Toast.LENGTH_SHORT).show();
+                       break;
+                   case R.id.action_favorites:
+                       Toast.makeText(MainActivity.this, "Favorites Add Clicked", Toast.LENGTH_SHORT).show();
+                       break;
+               }
+            }
+        });
     }
 
     @Override
