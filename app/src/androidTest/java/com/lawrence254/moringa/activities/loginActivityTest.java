0a76887e9f09ce36c130f451900d13de4ee0f5d9
Lawrence <lawrenslava1@gmail.com>
2018-11-02 11:52:47 +0300

    add directive to close the keyboard after typing out content in the fields in the loginActivityTest

--- app/src/androidTest/java/com/lawrence254/moringa/activities/loginActivityTest.java
+++ app/src/androidTest/java/com/lawrence254/moringa/activities/loginActivityTest.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -34,8 +35,8 @@ public class loginActivityTest {
     @Test
     public void correctLogin(){
 
-        onView(withId(R.id.username)).perform(click()).perform(typeText(username));
-        onView(withId(R.id.userpass)).perform(click()).perform(typeText(correctPass));
+        onView(withId(R.id.username)).perform(click()).perform(typeText(username),closeSoftKeyboard());
+        onView(withId(R.id.userpass)).perform(click()).perform(typeText(correctPass),closeSoftKeyboard());
 
         onView(withId(R.id.login)).perform(click());
 
