0847c7979ea6294a9dd78bc8a63ed78b7a1aa60f
Lawrence <lawrenslava1@gmail.com>
2018-11-05 15:46:54 +0300

    Return an empty URL

--- app/src/main/java/com/lawrence254/moringa/activities/Constants.java
+++ app/src/main/java/com/lawrence254/moringa/activities/Constants.java
@@ -2,4 +2,5 @@ package com.lawrence254.moringa.activities;
 
 public class Constants {
 //    public static String ARTICLES_URL = "https://api.myjson.com/bins/dkiga";
+    public static String ARTICLES_URL = "";
 }
