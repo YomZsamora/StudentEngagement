3f3f44876d97a315a2ae911d8cb10939c8d29bd4
Lawrence <lawrenslava1@gmail.com>
2018-11-06 10:03:40 +0300

    Check if frame container is displayed

--- app/src/androidTest/java/com/lawrence254/moringa/activities/loginActivityTest.java
+++ app/src/androidTest/java/com/lawrence254/moringa/activities/loginActivityTest.java
@@ -44,9 +44,9 @@ public class loginActivityTest {
         onView(withId(R.id.login)).check(matches(isDisplayed()));
     }
 
-//    @Test
-//    public void load_home_page(){
+    @Test
+    public void load_home_page(){
 //        onView(withId(R.id.login)).perform(click());
-//        onView(withId(R.id.navigation)).check(matches(isDisplayed()));
-//    }
+        onView(withId(R.id.frame_container)).check(matches(isDisplayed()));
+    }
 }
