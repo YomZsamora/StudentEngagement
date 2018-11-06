d0dccea3009c3d8def388e48e90bd712be60925e
Lawrence <lawrenslava1@gmail.com>
2018-11-06 16:18:50 +0300

    Add test to check whether the profile page can be accessed

--- app/src/androidTest/java/com/lawrence254/moringa/activities/loginActivityTest.java
+++ app/src/androidTest/java/com/lawrence254/moringa/activities/loginActivityTest.java
@@ -47,7 +47,7 @@ public class loginActivityTest {
 
     @Test
     public void load_home_page(){
-        onView(withId(R.id.login)).check(matches(isDisplayed()));
+//        onView(withId(R.id.login)).check(matches(isDisplayed()));
         onView(withId(R.id.login)).perform(closeSoftKeyboard(),scrollTo(),click());
         onView(withId(R.id.navigation)).check(matches(isDisplayed()));
     }
---
+++ app/src/androidTest/java/fragments/articleFragmentTest.java
@@ -0,0 +1,44 @@
+package fragments;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+
+import com.lawrence254.moringa.R;
+import com.lawrence254.moringa.activities.homeActivity;
+import com.lawrence254.moringa.activities.loginActivity;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class articleFragmentTest {
+    @Rule
+    public ActivityTestRule<homeActivity> mArticlesTestRule =
+            new ActivityTestRule<>(homeActivity.class,false,false);
+
+//    @Before
+//    public void setup(){
+//        mArticlesTestRule.launchActivity(MY_ACTIVITY_INTENT);
+//    }
+    @Test
+    public void profileVisibility(){
+//        onData(allOf(is(instanceOf(String.class)), ("Espresso"))) .perform(click());
+//        onView(allOf(withId(R.id.navigation), withText("Profile")))
+//                .check(matches(isDisplayed()));
+        onView(withId(R.id.profile)).check(matches(isDisplayed()));
+    }
+}
